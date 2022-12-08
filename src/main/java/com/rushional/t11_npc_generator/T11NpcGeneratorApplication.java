4fb8ed93eeceebe7c3453d511ac1e2ef57c425e5
Rushional <Rushional2@gmail.com>
2022-12-08 18:05:49 +0400

    First commit

---
+++ src/main/java/com/rushional/t11_npc_generator/T11NpcGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.rushional.t11_npc_generator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class T11NpcGeneratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(T11NpcGeneratorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rushional/t11_npc_generator/T11NpcGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rushional.t11_npc_generator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class T11NpcGeneratorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
